387d790515e21901745bcd0d167caf70a00e32e0
RodolfoContreras <55730927+RodolfoContreras@users.noreply.github.com>
2019-09-26 01:45:33 -0500

    Add files via upload

---
+++ Conversiones.py
@@ -0,0 +1,10 @@
+# Conversiones.py
+# Autor: Contreras Galvan Rodolfo Alejandro
+# Fecha de creación: 26/09/2019
+
+
+numero="1234"
+print("El tipo de dato de la variable numero es: ", type(numero))
+numero=int(numero)
+print("El tipo de dato de la variable numero convertida a entero es: ", type(numero))
+print ("El numero que se utilizo fue: ", numero)
