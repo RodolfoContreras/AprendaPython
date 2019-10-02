e881cdde41a493ab76a80fd2c3ed3d8ecf0ef147
RodolfoContreras <55730927+RodolfoContreras@users.noreply.github.com>
2019-10-02 02:24:56 -0500

    Add files via upload

---
+++ Nombre.py
@@ -0,0 +1,11 @@
+# Nombre.py
+# Autor: Contreras Galvan Rodolfo Alejandro
+# Fecha de creación: 02/09/2019
+
+nombre=input("Introduce tu nombre: ")
+apellido=input("Introduce tu apellido: ")
+
+nombrecompleto=nombre+" "+apellido
+nombrecompleto=nombrecompleto.upper()
+
+print("Tu nombre completo es: ",nombrecompleto)
